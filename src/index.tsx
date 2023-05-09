d79a87e98b9acd2c6656a69c2f69e0ca90e793ff
unknown <davidrobertson1997@gmail.com>
2023-05-08 23:12:32 -0500

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,6 +8,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+useEffect(() => {
+  console.log('index.tsx');
+})
 root.render(
   <BrowserRouter>
     <App />
